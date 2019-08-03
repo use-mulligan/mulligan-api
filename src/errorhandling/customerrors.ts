6da7821d784c6a4ed14fdfd2e3411df4bec54485
Austin Howlett <austinhowletttech@gmail.com>
2019-08-03 10:26:54 -0400

    added custom query and mutation errors mocking internal server errors

--- src/errorhandling/customerrors.ts
+++ src/errorhandling/customerrors.ts
@@ -3,10 +3,6 @@ export class AuthError extends Error {
     super("Account is not authorized.");
     // TODO: `Unique constraint violated on Account. Mutation did not complete. Field = ${field}`
     this.name = this.constructor.name;
-
-    // This clips the constructor invocation from the stack trace.
-    // It's not absolutely essential, but it does make the stack trace a little nicer.
-    Error.captureStackTrace(this, this.constructor);
   }
 }
 
@@ -15,9 +11,33 @@ export class UniqueConstraintViolationError extends Error {
     super(`Unique constraint violated on Account. Mutation did not complete.`);
     // TODO: `Unique constraint violated on Account. Mutation did not complete. Field = ${field}`
     this.name = this.constructor.name;
+  }
+}
+
+export class QueryFailedError extends Error {
+  constructor(ex) {
+    super(ex);
+
+    this.name = this.constructor.name;
+    this.message = ex.message;
+    this.stack = ex.stack;
 
     // This clips the constructor invocation from the stack trace.
     // It's not absolutely essential, but it does make the stack trace a little nicer.
-    Error.captureStackTrace(this, this.constructor);
+    ex.captureStackTrace(this, this.constructor);
+  }
+}
+
+export class MutationFailedError extends Error {
+  constructor(ex) {
+    super(ex);
+
+    this.name = this.constructor.name;
+    this.message = ex.message;
+    this.stack = ex.stack;
+
+    // This clips the constructor invocation from the stack trace.
+    // It's not absolutely essential, but it does make the stack trace a little nicer.
+    ex.captureStackTrace(this, this.constructor);
   }
 }
